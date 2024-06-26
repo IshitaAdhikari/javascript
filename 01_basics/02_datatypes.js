921864fff47e699b6039b3fc157c82e6ef334d7c
Ishita Adhikari <143061611+IshitaAdhikari@users.noreply.github.com>
2024-06-26 05:48:58 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+ "use strict"; //treating all JS code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+
+// types of datatypes
+
+// number -> 2 to power 53
+// bigint
+// stirng -> ""
+// boolean -> true/false
+// null
+// undefined  
+// symbol -> unique
+
+// object
+
+console.log(typeof null); // null is an object
