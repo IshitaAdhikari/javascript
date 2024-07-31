fc52c6b30f3e5b8c9d14b999add16b9ddbb60108
Ishita Adhikari <143061611+IshitaAdhikari@users.noreply.github.com>
2024-07-31 13:53:26 +0000

    commit

---
+++ 01_basics/04_comaprison.js
@@ -0,0 +1,11 @@
+console.log("2">1);
+console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined == 0);
+
+//comaprison and equality check both are diffrent 
+// === it also checks the data type
