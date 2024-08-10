e03410eb1d6dc4917467dc314ac01d10d6148b2b
Ishita Adhikari <143061611+IshitaAdhikari@users.noreply.github.com>
2024-08-10 14:37:30 +0000

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,14 @@
+// for loop
+
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    if(element == 5){
+        console.log("we got the 5");
+    }
+    console.log(element);
+}
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
